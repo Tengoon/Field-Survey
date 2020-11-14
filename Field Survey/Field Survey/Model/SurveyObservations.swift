856aabd755fa4db7dee805fb554a7ee70db605e0
Andrew Brown <andy.deanbrown@gmail.com>
2020-11-13 19:30:40 -0600

    Added codeables for json set

---
+++ Field Survey/Field Survey/Model/SurveyObservations.swift
@@ -0,0 +1,30 @@
+//
+//  SurveyObservations.swift
+//  Field Survey
+//
+//  Created by Andrew Brown on 11/13/20.
+//
+
+import Foundation
+
+enum Classification: String, Codable {
+    case amphibian
+    case bird
+    case fish
+    case insect
+    case mammal
+    case plant
+    case reptile
+}
+
+struct Observation: Codable {
+    var classification: Classification
+    var title: String
+    var description: String
+    var date: Date
+}
+
+struct FieldObservations: Codable {
+    var status: String
+    var observations: [Observation]
+}
